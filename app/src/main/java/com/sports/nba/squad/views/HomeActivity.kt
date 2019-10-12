7611e6f15856640d7ee8c43b53ea8471a509ac75
Chris Johnson <cljohnson4343@gmail.com>
2019-10-12 18:28:05 -0500

    Initial commit

---
+++ app/src/main/java/com/sports/nba/squad/views/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.sports.nba.squad.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sports.nba.squad.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
