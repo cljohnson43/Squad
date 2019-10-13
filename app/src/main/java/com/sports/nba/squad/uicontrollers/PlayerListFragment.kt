eaaa9f0bd884d791800a33f6f19913db6879a3e6
Chris Johnson <cljohnson4343@gmail.com>
2019-10-13 08:37:16 -0500

    Changed recyclerview layout manager to be grid.

--- app/src/main/java/com/sports/nba/squad/uicontrollers/PlayerListFragment.kt
+++ app/src/main/java/com/sports/nba/squad/uicontrollers/PlayerListFragment.kt
@@ -7,21 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.*
 import com.sports.nba.squad.R
 import com.sports.nba.squad.adapters.PlayersAdapter
 import com.sports.nba.squad.data.Player
 import com.sports.nba.squad.viewmodels.PlayersViewModel
 import kotlinx.android.synthetic.main.fragment_player_list.*
 
-class PlayerListFragment : Fragment(){
+class PlayerListFragment : Fragment() {
 
     private val playersViewModel: PlayersViewModel by lazy {
         activity?.let {
-            ViewModelProvider(it, ViewModelProvider.AndroidViewModelFactory(it.application)).get(PlayersViewModel::class.java)
+            ViewModelProvider(it, ViewModelProvider.AndroidViewModelFactory(it.application)).get(
+                PlayersViewModel::class.java
+            )
         } ?: throw Exception("Invalid Activity")
     }
 
@@ -38,7 +37,8 @@ class PlayerListFragment : Fragment(){
 
         val adapter = PlayersAdapter()
         rv_players.adapter = adapter
-        rv_players.layoutManager = LinearLayoutManager(this.context)
+        rv_players.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         subscribeUi(adapter)
     }
 
