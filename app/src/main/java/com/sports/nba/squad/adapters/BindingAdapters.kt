6219060e2745f380cc24a0b8d5708a52b35ef82b
Chris Johnson <cljohnson4343@gmail.com>
2019-10-14 21:12:12 -0500

    Adding text views to player details screen.

--- app/src/main/java/com/sports/nba/squad/adapters/BindingAdapters.kt
+++ app/src/main/java/com/sports/nba/squad/adapters/BindingAdapters.kt
@@ -3,7 +3,9 @@ package com.sports.nba.squad.adapters
 import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.sports.nba.squad.data.Player
 import com.sports.nba.squad.utils.Image
 
 @BindingAdapter("backgroundColor")
@@ -14,4 +16,9 @@ fun backgroundColor(view: View, color: String) {
 @BindingAdapter("fromUrl")
 fun fromUrl(imageView: ImageView, url: String) {
     Image.fromUrl(imageView, url)
+}
+
+@BindingAdapter("playerLabel")
+fun playerLabel(textView: TextView, player: Player) {
+    textView.text = "#${player.number} ${player.firstName.toUpperCase().trim()} ${player.lastName.toUpperCase().trim()}"
 }
